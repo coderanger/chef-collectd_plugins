d8e264da9ad982dc75978e48c14a9323db553378
Noah Kantrowitz <noah@coderanger.net>
2012-02-21 18:53:45 -0800

    Initial commit of collectd_plugins cookbook.

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "Noan Kantrowitz"
+maintainer_email "noah@coderanger.net"
+license          "Apache 2.0"
+description      "Configure collectd plugins"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "1.0.0"
+depends          "collectd"
+supports         "ubuntu"
